6ec8bbccd2ae9cfa2147cb916f37365123e6a077
veli_legenda <116480916+veli228339@users.noreply.github.com>
2026-04-29 04:28:51 +0700

    Изменил исключения для БотДляПроекта.py
    в кратце: теперь бот отвечает на все смс.

--- БотДляПроекта.py
+++ БотДляПроекта.py
@@ -5,27 +5,38 @@ s = {}
 
 @b.message_handler(content_types=['text'])
 def u(m):
-    c = m.chat.id
-    x = m.text
+    try:
+        c = m.chat.id
+        x = m.text
 
-    if x == "/start":
-        k = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-        k.add(telebot.types.KeyboardButton("Оставить заявку"), telebot.types.KeyboardButton("Команда"))
-        b.send_message(c, 'Добро пожаловать! Это telegram-бот проекта "Так Сяк".', reply_markup=k)
-        return
+        if x == "/start" or x == "Привет!":
+            k = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+            k.add(telebot.types.KeyboardButton("Оставить заявку"), telebot.types.KeyboardButton("Команда"),telebot.types.KeyboardButton("Привет!"))
+            b.send_message(c, 'Добро пожаловать! Это telegram-бот проекта "Так Сяк".', reply_markup=k)
+            return
 
-    if x == "Команда":
-        b.send_message(c, "Портфолио команды из 5 человек.")
-        return
+        if x == "Команда":
+            b.send_message(c, "Портфолио команды из 5 человек.")
+            return
 
-    if x == "Оставить заявку":
-        s[c] = True
-        b.send_message(c, "ТЗ:")
-        return
+        if x == "Оставить заявку":
+            s[c] = True
+            b.send_message(c, "ТЗ:")
+            return
 
-    if s.get(c):
-        s[c] = False
-        b.send_message(c, "Заявка принята.")
-        print(x)
+        if s.get(c):
+            s[c] = False
+            b.send_message(c, "Заявка принята.")
+            print(x)
+            return
+            
+        b.send_message(c, "Выберите из меню.")
+        
+    except Exception as e:
+        print(e)
 
-b.polling(none_stop=True)
+while True:
+    try:
+        b.polling(none_stop=True)
+    except Exception as e:
+        print(e)
